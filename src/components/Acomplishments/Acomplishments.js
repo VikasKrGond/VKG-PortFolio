e5a1c5f577fe2171807422415b2b1c20ea587949
Vikas Kumar Gond <76789978+VikasKrGond@users.noreply.github.com>
2021-11-24 19:31:24 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -7,7 +7,7 @@ const data = [
   { number: 3000, text: 'Linkdin Followers', },
   { number: 35, text: 'Open Source Projects'},
   { number: 10, text: 'Frameworks Learnt', },
-  { number: 500, text: 'Github Contributions', }
+  { number: 600, text: 'Github Contributions', }
 ];
 
 const Acomplishments = () => (

01cb713afcbadf8f16c489413602314ab300e5ed
Vikas Kumar Gond <76789978+VikasKrGond@users.noreply.github.com>
2022-01-10 23:41:25 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -30,6 +30,11 @@ const Header = () =>  (
         <NavLink>About</NavLink>
       </Link>
       </li>
+ <li>
+      <Link href="https://vkgpersonalblog.netlify.app/">
+        <NavLink>Blog</NavLink>
+      </Link>
+      </li>
     </Div2>
     <Div3>
       <SocialIcons href="https://github.com/VikasKrGond">
